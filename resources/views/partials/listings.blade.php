0d9537eebb0894f70f6bc067b6e340c112309ba9
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-10-12 19:49:38 -0600

    skills

--- resources/views/partials/listings.blade.php
+++ resources/views/partials/listings.blade.php
@@ -9,9 +9,13 @@
             <div class="inline-flex">
                 <ul>
                     @foreach($position->skills as $skill)
-                    <a class="text-indigo-700 hover:text-indigo-500"  style="transition: all 0.2s" href="{{route('skills.show', $skill->id)}}">
-                        <li class="inline-block my-2 p-2 rounded bg-indigo-200">{{$skill->name}}</li>
-                    </a>
+
+                        <li class="inline-block m-2 rounded bg-indigo-200">
+                            <a class="p-2 text-indigo-700 hover:text-indigo-500"  style="transition: all 0.2s" href="{{route('skills.show', $skill->id)}}">
+                                {{$skill->name}}
+                            </a>
+                        </li>
+
                     @endforeach
                 </ul>
             </div>
